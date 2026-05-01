862ff2e739301f46bd0d2d36cb0b78a6f8c946f5
Rianne <lpb.rianne@gmail.com>
2026-04-30 20:16:52 -0600

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -2,26 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite';
 import path from 'path';
-import fs from 'fs';
-import { fileURLToPath } from 'url';
-const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
-
-//get projects
-function getProjects() {
-  const entries: Record<string, string> = {};
-  const projectsDir = path.resolve(__dirname, 'public', 'projects');
-
-  // Read directories in 'projects'
-  fs.readdirSync(projectsDir).forEach(dir => {
-    const indexPath = path.resolve(projectsDir, dir, 'index.html');
-    if (fs.existsSync(indexPath)) {
-      entries[dir] = indexPath;
-    }
-  });
-
-  return entries;
-}
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -35,12 +16,5 @@ export default defineConfig({
     react(),
     tailwindcss()
   ],
-  build: {
-    rollupOptions: {
-     input:  getProjects(),
-     
-     
-      
-    }
-  }
+
 })
