4965e5b3c92b4513ea7a1c535d82e86cf8184cd3
Rianne <lpb.rianne@gmail.com>
2026-05-05 13:27:01 -0600

    video

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import HomePage from './pages/Home';
 import ProjectPage from './pages/Project';
+import VideoPage from './pages/Video';
 
 import { ThemeProvider } from "./context/ThemeContext";
 
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/project/:slug" element={<ProjectPage />} />
+        <Route path="/fall" element={<VideoPage title="Fall 2026" url="./fall26.mp4" videoProps={{ autoPlay: true, muted: true, loop: true }}  />} />
       </Routes>
     </ThemeProvider>
 
---
+++ src/pages/Video.tsx
@@ -0,0 +1,30 @@
+import { Link } from "react-router-dom";
+import Navbar from "../components/Navbar";
+
+export default function ProjectPage({ title, url, videoProps }: { title: string; url: string, videoProps?: React.VideoHTMLAttributes<HTMLVideoElement> }) {
+
+  return (
+    <>
+      <Navbar />
+      <main className="w-full p-8 bg-background">
+
+        <div className="flex flex-row justify-between mb-8">
+          <Link to="/" className="back-link">
+            ← Back to projects
+          </Link>
+        </div>
+
+        <div className="flex w-full flex-col ">
+
+          <h1 className="text-6xl font-bold mb-4">{title}</h1>
+          
+          <video className="flex w-full" width={'100%'} height={'500'} {...videoProps}>
+            <source src={url} type="video/mp4" />
+            Your browser does not support the video tag.
+          </video>  
+
+        </div>
+      </main>
+    </>
+  );
+}
